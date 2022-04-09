ec5daa4aa3e496543afb8b5e62c920549a3ed535
Kederal <87231854+Kederal@users.noreply.github.com>
2022-04-08 20:10:22 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,19 @@
 
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+  vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+  wait(1)
+  vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
+local ScriptURL = "https://raw.githubusercontent.com/Kederal/Fate-Scripts/main/SupportedGames/"
+
+local SupportedGames = {
+    [7788040584] = 'PetLegacy';
+}
+
+for i,v in next, SupportedGames do
+if i == game.PlaceId or i == game.GameId then
+loadstring(game:HttpGet(ScriptURL .. v .. '.lua'))()       
+end
+end
